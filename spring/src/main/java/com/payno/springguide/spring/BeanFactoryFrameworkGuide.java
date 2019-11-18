11806c484d73c7b4ddbc03ffc2a828ade054a0d5
BigPayno <1361098889@qq.com>
2019-11-18 11:25:13 +0800

    熟悉了BeanFactory和ApplicationContext等接口 更加熟悉ResourceLoader和Resource框架 了解Resolver模式的使用 加深了面向接口编程的思维

--- spring/src/main/java/com/payno/springguide/spring/BeanFactoryFrameworkGuide.java
+++ spring/src/main/java/com/payno/springguide/spring/BeanFactoryFrameworkGuide.java
@@ -1,6 +1,17 @@
 package com.payno.springguide.spring;
 
+import com.google.common.collect.ImmutableList;
+import lombok.Data;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * @author payno
@@ -20,9 +31,46 @@ import org.springframework.beans.factory.BeanFactory;
  *              beanPostProcessors
  *              ->BeanPostProcessor 在对象初始化前进行操作
  *              embeddedValueResolvers
- *  BeanPostProcessor
- *  BeanFactoryProcessor
+ *  BeanPostProcessor的使用
+ *  BeanFactoryProcessor的使用
  */
+@Configuration
 public class BeanFactoryFrameworkGuide {
+    @Component("person")
+    @Data
+    public static class Person{
+        private String name;
+        private String pass;
+    }
 
+    @Component
+    public static class FactoryPost implements BeanFactoryPostProcessor{
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+            System.out.println("|||||||||post process before init factory!!|||||||||");
+            ImmutableList.copyOf(configurableListableBeanFactory.getBeanDefinitionNames()).forEach(name->{
+                System.out.println("[name]:"+name);
+            });
+            System.out.println("|||||||||post process before init factory!!|||||||||");
+            BeanDefinition beanDefinition=configurableListableBeanFactory.getBeanDefinition("person");
+            MutablePropertyValues propertyValues=beanDefinition.getPropertyValues();
+            propertyValues.addPropertyValue("name","payno");
+        }
+    }
+
+    @Component
+    public static class BeanPost implements BeanPostProcessor{
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            System.out.printf("beanName[%s]:[%s]%n",beanName,bean.toString());
+            return bean;
+        }
+    }
+    public static class BeanFactoryPostTest{
+        public static void main(String[] args) {
+            AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext(BeanFactoryFrameworkGuide.class);
+            Person person=applicationContext.getBean("person",Person.class);
+            System.out.println(person);
+        }
+    }
 }

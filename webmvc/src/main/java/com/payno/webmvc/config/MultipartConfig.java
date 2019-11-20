e6eeac1add1b751bb5711976cae3b2592cd6d47a
BigPayno <1361098889@qq.com>
2019-11-20 17:52:02 +0800

    关于SpringMvc如何配置下载文件存储路径的Config

---
+++ webmvc/src/main/java/com/payno/webmvc/config/MultipartConfig.java
@@ -0,0 +1,28 @@
+package com.payno.webmvc.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * @author payno
+ * @date 2019/11/20 17:28
+ * @description
+ */
+@Configuration
+public class MultipartConfig {
+    /**
+     * 配置Multipart默认上传路径
+     */
+    @Bean
+    MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory=new MultipartConfigFactory();
+        /**
+         * 相对jar的路径  / 当前路径
+         */
+        factory.setLocation("/");
+        return factory.createMultipartConfig();
+    }
+}

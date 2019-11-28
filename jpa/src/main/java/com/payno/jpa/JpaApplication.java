77c24014e440edc673e318bfaf67dc9a368ac5b1
BigPayno <1361098889@qq.com>
2019-11-28 16:18:32 +0800

    补充下

---
+++ jpa/src/main/java/com/payno/jpa/JpaApplication.java
@@ -0,0 +1,15 @@
+package com.payno.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+
+}
---
+++ jpa/src/test/java/com/payno/jpa/JpaApplicationTests.java
@@ -0,0 +1,67 @@
+package com.payno.jpa;
+
+import com.payno.jpa.audit.entity.Audit1;
+import com.payno.jpa.audit.entity.Audit2;
+import com.payno.jpa.audit.repo.Audit1Repo;
+import com.payno.jpa.audit.repo.Audit2Repo;
+import com.payno.jpa.demo.Data;
+import com.payno.jpa.demo.DataRepo;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = JpaApplication.class)
+class JpaApplicationTests {
+
+    @Autowired
+    Audit1Repo audit1Repo;
+    @Autowired
+    Audit2Repo audit2Repo;
+    @Autowired
+    DataRepo dataRepo;
+
+    /**
+     * 插入时自带时间
+     */
+    @Test
+    void audit1Save() throws Exception{
+        Audit1 audit1=Audit1.builder().description("nothing").title("no").build();
+        audit1Repo.save(audit1);
+        System.out.println(audit1);
+    }
+
+    /**
+     * 更新时，数据库Version及时间发生变化
+     * 应该是生成代理对象persist，所以代码中的对象时间及版本无变化
+     */
+    @Test
+    void audit1Update(){
+        Audit1 audit1=audit1Repo.findById(Long.valueOf(9)).get();
+        audit1.setTitle("new");
+        System.out.println(audit1);
+        audit1Repo.save(audit1);
+        System.out.println(audit1);
+    }
+
+    @Test
+    void audit2CreateBy(){
+        Audit2 audit2=Audit2.builder().title("test").build();
+        audit2Repo.save(audit2);
+        System.out.println(audit2);
+    }
+
+    /**
+     * 看看忽略路径是否有用,以及抽象类的某些预定功能
+     */
+    @Test
+    void data(){
+        dataRepo.save(new Data());
+        dataRepo.save(new Data());
+        Data example=new Data();
+        example.setType(2);
+        dataRepo.findAll(example.toExample()).forEach(System.out::println);
+    }
+}

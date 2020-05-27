09b27473804b4cad01b5a2a47f67db0dcbcbcfaa
BigPayno <1361098889@qq.com>
2020-05-27 11:05:11 +0800

    关于Spring对于Redis的相关UML

--- redis/src/main/java/com/payno/redis/clients/config/ClusterAndSentinel.java
+++ redis/src/main/java/com/payno/redis/clients/config/ClusterAndSentinel.java
@@ -1,5 +1,10 @@
 package com.payno.redis.clients.config;
 
+import org.crazycake.shiro.IRedisManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.data.redis.core.RedisTemplate;
+
 /**
  * @author payno
  * @date 2019/12/1 16:12
@@ -16,4 +21,44 @@ package com.payno.redis.clients.config;
  *
  */
 public class ClusterAndSentinel {
+    /**
+     *      哨兵模式(高可用：监控，自动转移)
+     *      sentinel:
+     *          master:
+     *          nodes:
+     *
+     *      集群模式(高并发：为了解决单机Redis容量有限的问题)
+     *      cluster:
+     *          nodes:
+     *
+     */
+    public void patterns() {
+        RedisProperties redisProperties = new RedisProperties();
+        //  具体配置模式结合autoConfig 可以了解spring配置方式
+        if(redisProperties.getCluster()==null&&redisProperties.getSentinel()==null){
+            System.out.println("单机Redis");
+        }else if(redisProperties.getCluster()!=null){
+            System.out.println("集群Redis");
+        }else{
+            System.out.println("哨兵Redis");
+        }
+    }
+
+    /**
+     *  shiro<>--->CacheManager(shiro)
+     *                  <|-----RedisCacheManager(crazyCake)
+     *                              <>----->IRedisManager(crazyCake)
+     *
+     *  IRedisManager <|-----WorkAloneRedisManager<|-----RedisSentinelManager（哨兵）/RedisManager(单机)
+     *                <|-----RedisClusterManager
+     *
+     *  需要注意的是crazyCake的IRedisManager的实现大多依赖Jedis
+     *  如果Spring配置了其他的连接Redis的工厂，服务就会创建多个不同的jedis工厂，甚至可能因为没有jedis对象而出问题
+     *  为了解决这样的问题，需要深入RestTemplate的创建方式，并以RestTemplate的创建方式来实现IRedisManager
+     *  这样就可以实现Spring和crazyCake的兼容
+     */
+    public void cacheManager(IRedisManager redisManager){
+        RedisCacheManager cacheManager = new RedisCacheManager();
+        cacheManager.setRedisManager(redisManager);
+    }
 }
---
+++ redis/src/main/java/com/payno/redis/clients/config/TemplateAndConnection.java
@@ -0,0 +1,25 @@
+package com.payno.redis.clients.config;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author payno
+ * @date 2020/5/27 10:49
+ * @description
+ */
+public class TemplateAndConnection {
+
+    public void redisTemplate(){
+        /**
+         * @see RedisTemplate.png
+         *      关于RedisTemplate的设计UML
+         */
+    }
+
+    public void redisConnection(){
+        /**
+         * @see RedisConnection.png
+         *      关于RedisConnection的设计UML
+         */
+    }
+}
